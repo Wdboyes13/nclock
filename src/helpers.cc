1e7f2200281b7e9a52756c5ee1965bbbff9525bd
44 <urbuina@gmail.com>
2026-05-02 06:59:00 +0300

    Update helpers.cc

--- src/helpers.cc
+++ src/helpers.cc
@@ -1,5 +1,7 @@
 #include <algorithm>
 #include <format>
+#include <sstream>
+#include <cmath>
 #include "clock.hpp"
 
 Time App::curr_time() {
@@ -24,9 +26,7 @@ int App::colused(const std::string& str) {
     std::string line;
 
     while (std::getline(stream, line)) {
-        std::wstring ws(line.begin(), line.end());
-        int w = wcswidth(ws.c_str(), ws.size());
-        max_cols = std::max(max_cols, w == -1 ? (int)line.size() : w);
+        max_cols = std::max(max_cols, static_cast<int>(line.size()));
     }
 
     return max_cols;
@@ -43,9 +43,18 @@ void App::mvwprintfig(WINDOW* win, int row, int col, const std::string& fig_str)
 
 long App::local_utcoff() {
     auto now = time(NULL);
-    auto time = localtime(&now);
-
-    return time->tm_gmtoff;
+    auto t = localtime(&now);
+    char buf[6];
+    strftime(buf, sizeof(buf), "%z", t);
+    
+    std::string tz(buf);
+    if (tz.length() == 5) {
+        int hours = std::stoi(tz.substr(1, 2));
+        int minutes = std::stoi(tz.substr(3, 2));
+        long offset = (hours * 3600) + (minutes * 60);
+        return tz[0] == '-' ? -offset : offset;
+    }
+    return 0;
 }
 
 std::string App::format_tzoff(long off_sec) {
