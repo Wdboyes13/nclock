970fdfb0dee373464b59a2d6218f092229541d14
44 <urbuina@gmail.com>
2026-05-04 22:11:19 +0300

    Update helper_uis.cc

--- src/helper_uis.cc
+++ src/helper_uis.cc
@@ -2,53 +2,77 @@
 
 WINDOW* App::create_overlay() {
     WINDOW* overlay = newwin(wsz.r, wsz.c, 0, 0);
-    wbkgd(overlay, COLOR_PAIR(CPAIR_OVERLAY));
-
     werase(overlay);
     wrefresh(overlay);
+    wbkgd(overlay, COLOR_PAIR(app_constants::CPAIR_OVERLAY));
+    for (int r = 0; r < wsz.r; r++) {
+        for (int c = 0; c < wsz.c; c++) {
+            mvwaddch(overlay, r, c, ' ' | A_DIM);
+        }
+    }
     return overlay;
 }
 
 void App::do_error(const char* err) {
     try {
-        auto overlay = create_overlay();
-        int dh = 15, dw = 45;
+        const int old_curs = curs_set(0);
+        const auto overlay = create_overlay();
+        const int dh = 15, dw = 45;
         WINDOW* dialog = newwin(dh, dw, (wsz.r - dh) / 2, (wsz.c - dw) / 2);
-        wbkgd(dialog, COLOR_PAIR(CPAIR_OVERLAY));
+        wbkgd(dialog, COLOR_PAIR(app_constants::CPAIR_OVERLAY));
         box(dialog, 0, 0);
-        cblock(dialog, CPAIR_ERR, [&]() {
+        cblock(dialog, app_constants::CPAIR_ERR, [&]() {
             mvwprintw(dialog, 2, 3, "!!Error!!");
         });
-        mvwprintw(dialog, 3, 5, err);
 
-        int btn_row = dh - 3, btn_col = (dw - 6) / 2;
-        wattron(dialog, A_REVERSE);
-        mvwprintw(dialog, btn_row, btn_col, "[ OK ]");
-        wattroff(dialog, A_REVERSE);
+        int err_row = 4;
+        int err_col = 3;
+        int max_msg_w = dw - 6;
+        std::string err_msg = err;
+        while (static_cast<int>(err_msg.size()) > max_msg_w) {
+            mvwprintw(dialog, err_row++, err_col, "%-.*s", max_msg_w, err_msg.c_str());
+            err_msg = err_msg.substr(max_msg_w);
+        }
+        if (!err_msg.empty()) {
+            mvwprintw(dialog, err_row, err_col, "%s", err_msg.c_str());
+        }
+
+        const int btn_row = dh - 3;
+        const int btn_col = (dw - 6) / 2;
+        cblock(dialog, app_constants::CPAIR_ERR, [&]() {
+            wattron(dialog, A_REVERSE);
+            mvwprintw(dialog, btn_row, btn_col, "[ OK ]");
+            wattroff(dialog, A_REVERSE);
+        });
+
+        wnoutrefresh(dialog);
+        doupdate();
 
         keypad(dialog, true);
-        wrefresh(dialog);
+        flushinp();
 
         while (true) {
-            int c = wgetch(dialog);
-            if (c == '\n' || c == ' ' || c == ctrl('q')) {
+            const int c = wgetch(dialog);
+            if (c == '\n' || c == ' ' || c == ctrl('q') || c == 27) {
                 break;
             }
         }
 
         delwin(dialog);
         delwin(overlay);
+        curs_set(old_curs);
 
         touchwin(stdscr);
-        touchwin(barwin);
         if (twin) {
             touchwin(twin);
+        }
+        if (barwin) {
+            touchwin(barwin);
+        }
+        if (twin) {
             wrefresh(twin);
         }
         ::refresh();
-    } catch (std::exception& e) {
-        endwin();
-        printf("Fatal error: could not show error window");
-        std::exit(1);
+    } catch (const std::exception& e) {
     }
 }
